80193e9b4f9b462a10e72eed2741df17b3dba0d7
CesarCarmona <clcg852@gmail.com>
2024-04-13 17:20:24 -0600

    update salario.cpp

--- Ejercicios-C++/Menu/Salario/salario.cpp
+++ Ejercicios-C++/Menu/Salario/salario.cpp
@@ -7,8 +7,10 @@ using namespace std;
 int main() {
   // Variables para almacenar las horas trabajadas totales y para las horas extras
   int horas_trabajadas = 0, horas_extras;
-  // Las tarifas como no varian son constantes
-  const float TARIFA_NORMAL = 350.00f, TARIFA_EXTRA = 1.5 * TARIFA_NORMAL, RETENCION_1 = .12, RETENCION_2 = .18;
+  // Las tarifas y retenciones como no varian son constantes
+  const float TARIFA_NORMAL = 350.00f;
+  const float TARIFA_EXTRA = 1.5 * TARIFA_NORMAL; 
+  const float RETENCION_1 = .12f, RETENCION_2 = .18f;
   // Variables de punto decimal para almacenar los sueldos
   double sueldo_bruto, sueldo_neto, retenciones;
   
@@ -17,15 +19,14 @@ int main() {
   while (horas_trabajadas <= 0) {
         cout << "Por favor, introduce un numero de horas valido: ";
         /* Leemos las horas trabajadas totales hasta que se
-          introduzca una correcta
-        */
+          introduzca una correcta */
         cin >> horas_trabajadas;
         // Limpiamos el búfer de entrada de datos
         cin.ignore();
     }
   if (horas_trabajadas <= 40) {
     /* Si las hrs trabajadas son menores o iguales a 40, entonces 
-      calculamos el sueldo bruto con la tarifa normal de 350.00*/
+      calculamos el sueldo bruto con la tarifa normal de 350.00 */
     sueldo_bruto = horas_trabajadas * TARIFA_NORMAL;
   } else {
     /* Si las hrs superan las 40 hrs, restamos 40 hrs a la cantidad
@@ -33,7 +34,7 @@ int main() {
     horas_extras = horas_trabajadas - 40;
     /* Calculamos el sueldo bruto, primero la tarifa normal para la cantidad
       maxima de hrs que son 40, y luego con tarifa extra para las 
-      horas extra, al final sumamos el sueldo de ambas tarifas*/
+      horas extra, al final sumamos el sueldo de ambas tarifas */
     sueldo_bruto = (40 * TARIFA_NORMAL) + (horas_extras * TARIFA_EXTRA);
   }
   if (sueldo_bruto <= 10000.00f) {
@@ -46,8 +47,7 @@ int main() {
       neto.
       Calculamos las retenciones que es del %12 sobre el sueldo bruto.
       Después restamos las retenciones al sueldo bruto para obtener el 
-      sueldo neto
-      */
+      sueldo neto */
     retenciones = RETENCION_1 * sueldo_bruto;
     sueldo_neto = sueldo_bruto - retenciones;
   } else {
@@ -55,14 +55,14 @@ int main() {
       deducimos una retencion del %18 para obtener el sueldo neto
       Calculamos las retenciones que es del %18 sobre el sueldo bruto.
       Después restamos las retenciones al sueldo bruto para obtener el 
-      sueldo neto
-      */
+      sueldo neto */
     retenciones = RETENCION_2 * sueldo_bruto;
     sueldo_neto = sueldo_bruto - retenciones;
   }
+  // Se utiliza fixed y setprecision para formatear los números de punto flotante
   cout << "Su sueldo bruto fue de: $" << fixed << setprecision(2) << sueldo_bruto << endl;
-  cout << "Retenciones: $" << retenciones << endl;
-  cout << "Menos retenciones, su sueldo neto es de: $" << sueldo_neto << endl;
+  cout << "Retenciones: $" << fixed << setprecision(2) << retenciones << endl;
+  cout << "Menos retenciones, su sueldo neto es de: $" << fixed << setprecision(2) << sueldo_neto << endl;
 
   return 0;
 }
